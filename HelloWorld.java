23cab1ad7f19270989a4e5f75c3b1d33ba6b1476
abangiraekyomuhendo <abangiraprecious@gmail.com>
2025-09-30 18:02:20 +0300

    hello world

---
+++ HelloWorld.java
@@ -0,0 +1,29 @@
+// This is a single line comment
+ /*
+ This is a multiline comment 
+ */
+
+/**
+ * This is Javadoc comment used for documentation
+ */
+
+public class HelloWorld {
+    /**
+     * main method - the entry point of any java application
+     */
+
+    /*
+    public - access modifier - means the class is accessible from anywhere
+    class - keywowrd to define a class (blueprint for objects)
+    class name must match filename
+    curly braces define the class     */
+
+    public static void main(String[] args) {
+        /*static - belongs to the class rather than instances (no objects neede) 
+        void - return type - doesn't return any value
+        String[] args - parameter - array of strings for command line arguments
+        */
+
+        System.out.println("Hello World"); //This line prints out. "ln" prints on a new line 
+    }
+}
