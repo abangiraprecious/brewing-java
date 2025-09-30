f208df165a2e75fca8c06d929d9e30120044a70a
abangiraekyomuhendo <abangiraprecious@gmail.com>
2025-09-30 18:15:35 +0300

    calc

---
+++ Calc.java
@@ -0,0 +1,9 @@
+public class Calc {
+        //Define the method
+        // a method is a block of code that performs a specific task and is defined within a task
+        public int add(int a, int b) {
+            return a + b;
+        }
+
+     //i'll be right bacck
+}
